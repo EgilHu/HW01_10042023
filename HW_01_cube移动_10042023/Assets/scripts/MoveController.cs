b77fd108c1e4586bffc35a7248a9c563785efd83
EgilHu <qingccch@gmail.com>
2023-11-01 21:56:05 +0800

    CameraRotatewithCube_Debug

--- HW_01_cube移动_10042023/Assets/scripts/MoveController.cs
+++ HW_01_cube移动_10042023/Assets/scripts/MoveController.cs
@@ -6,8 +6,6 @@ public class CubeController : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public float rotationSpeed = 90.0f;
-    public Transform cameraTransform;
-
     private Vector3 moveDirection;
 
     void Update()
@@ -16,11 +14,11 @@ public class CubeController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         // 计算移动方向
-        Vector3 forward = cameraTransform.forward;
+        Vector3 forward = transform.forward;
         forward.y = 0;
         forward.Normalize();
 
-        Vector3 right = cameraTransform.right;
+        Vector3 right = transform.right;
         right.y = 0;
         right.Normalize();
 
@@ -36,11 +34,5 @@ public class CubeController : MonoBehaviour
             // 移动Cube
             transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
         }
-
-        // 让相机自动跟随Cube的旋转
-        cameraTransform.rotation = Quaternion.LookRotation(-moveDirection, Vector3.up);
-
-        // 更新相机位置
-        cameraTransform.position = transform.position + Vector3.up * 2.0f - moveDirection * 5.0f;
     }
 }
